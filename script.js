93e9cb8a4e06d33cdfe8805b9e2fcfa271752367
Pierre <97318250+Gaufrinette@users.noreply.github.com>
2023-06-22 16:15:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Connexion à la chatbox
+var username = prompt("Veuillez entrer votre pseudonyme :");
+
+// Événement déclenché lorsqu'un message est envoyé
+document.getElementById("send").addEventListener("click", function() {
+  var messageInput = document.getElementById("message");
+  var message = messageInput.value;
+  sendMessage(username, message);
+  messageInput.value = "";
+});
+
+// Événement déclenché lorsqu'une touche est pressée dans le champ de saisie du message
+document.getElementById("message").addEventListener("keypress", function(event) {
+  if (event.keyCode === 13) { // 13 correspond à la touche "Entrée"
+    document.getElementById("send").click();
+  }
+});
+
+// Fonction pour envoyer un message à la chatbox
+function sendMessage(username, message) {
+  var chatlog = document.getElementById("chatlog");
+  var newMessage = document.createElement("div");
+  newMessage.innerHTML = "<strong>" + username + ":</strong> " + message;
+  chatlog.appendChild(newMessage);
+  chatlog.scrollTop = chatlog.scrollHeight; // Fait défiler la chatbox automatiquement
+}
